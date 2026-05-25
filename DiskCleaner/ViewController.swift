04f6be0dddb01746d1c608fcb99feeff0aada62a
xinfeng <xf93328@163.com>
2026-05-25 23:40:46 +0800

    Initial Commit

---
+++ DiskCleaner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DiskCleaner
+//
+//  Created by xinfeng on 2026/5/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ DiskCleanerTests/DiskCleanerTests.swift
@@ -0,0 +1,19 @@
+//
+//  DiskCleanerTests.swift
+//  DiskCleanerTests
+//
+//  Created by xinfeng on 2026/5/25.
+//
+
+import Testing
+@testable import DiskCleaner
+
+struct DiskCleanerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
